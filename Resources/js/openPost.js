1f934105e2a176e5a8da0c7cc99a170878e46e63
Hamza <muhammadhamzam1486@gmail.com>
2021-05-01 11:34:50 +0500

    DESIGN article page

--- Resources/js/openPost.js
+++ Resources/js/openPost.js
@@ -1,5 +1,5 @@
 export default function openPost(e) {
   e.preventDefault();
 
-  document.location = `/${this.dataset.href}`;
+  document.location = `/${this.dataset.href}?id=${this.dataset.id}`;
 }
